c46dedc335b4f5c35c4bef2154ee4c39fc449225
LahmarTabai <79151540+LahmarTabai@users.noreply.github.com>
2022-02-17 11:56:34 +0100

    modif

--- js/acceuil.js
+++ js/acceuil.js
@@ -1,37 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    
     let btn = document.querySelectorAll(".toggle");
-    let res = document.querySelectorAll(".hidden");
-
-    let icouvert = document.querySelectorAll("ouvrir");
-
-
-
+    let body = document.querySelector('body');
+    let header = document.querySelector('header');
     let icoferm = '<i class="fas fa-times fermer"></i>';
-
-
-
     
     for (let i = 0; i < btn.length; i++) {
         btn[i].addEventListener("click", function (e) {
-
-            res[i].classList.toggle("show"); 
-
-            if (btn[i].innerHTML.match('<i class="fas fa-plus ouvrir"></i>')) {
+            if (btn[i].innerHTML.match('<i class="fas fa-bars ouvrir"></i>')) {
                 btn[i].innerHTML = icoferm;
+                header.style.background="#000";
             }else if (btn[i].innerHTML.match('<i class="fas fa-times fermer"></i>')) {
-                btn[i].innerHTML = '<i class="fas fa-plus ouvrir"></i>';
+                btn[i].innerHTML = '<i class="fas fa-bars ouvrir"></i>';
+                header.style.background="";
             }
-            
-          
-            
-            
+            body.classList.toggle("open");
         })
     };
+      
+    
 
 
-   
-  
+
+
+
+
+
+
+
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Projet Controle Copie Netflix
+
+## Page de Connexion / Formulaire / Accueil :
+
+### Pour le code html minimal
+
+#[Tux, the Linux mascot](./asset/tabai.jpg)
+
+## Les notions de dévéloppment
+* HTML<sup>5</sup>
+* CSS<sub>3<sub>
+* js
+* PHP
+* Versioning
+
+Voir la Page Web *[Page web](https://lahmartabai.github.io/Netflix/)*.
