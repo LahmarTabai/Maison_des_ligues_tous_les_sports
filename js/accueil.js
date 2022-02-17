f0782b0f89ddab3ebb3accffbb8c34f7a9161408
LahmarTabai <79151540+LahmarTabai@users.noreply.github.com>
2022-02-17 14:54:28 +0100

    modif

--- js/accueil.js
+++ js/accueil.js
@@ -1,52 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
     let btn = document.querySelectorAll(".toggle");
     let body = document.querySelector('body');
     let header = document.querySelector('header');
     let icoferm = '<i class="fas fa-times fermer"></i>';
-    
+
     for (let i = 0; i < btn.length; i++) {
         btn[i].addEventListener("click", function (e) {
             if (btn[i].innerHTML.match('<i class="fas fa-bars ouvrir"></i>')) {
                 btn[i].innerHTML = icoferm;
-                header.style.background="#000";
-            }else if (btn[i].innerHTML.match('<i class="fas fa-times fermer"></i>')) {
+                header.style.background = "#000";
+            } else if (btn[i].innerHTML.match('<i class="fas fa-times fermer"></i>')) {
                 btn[i].innerHTML = '<i class="fas fa-bars ouvrir"></i>';
-                header.style.background="";
+                header.style.background = "";
             }
             body.classList.toggle("open");
-        })
+        });
     };
-      
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
--- js/app.js
+++ js/app.js
@@ -1,55 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let btn = document.querySelectorAll(".toggle");
+  let res = document.querySelectorAll(".hidden");
 
-    let btn = document.querySelectorAll(".toggle");
-    let res = document.querySelectorAll(".hidden");
+  let icouvert = document.querySelectorAll("ouvrir");
 
-    let icouvert = document.querySelectorAll("ouvrir");
+  let icoferm = '<i class="fas fa-times fermer"></i>';
 
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function (e) {
 
+      res[i].classList.toggle("show");
 
-    let icoferm = '<i class="fas fa-times fermer"></i>';
-
-
-
-    
-    for (let i = 0; i < btn.length; i++) {
-        btn[i].addEventListener("click", function (e) {
-
-            res[i].classList.toggle("show"); 
-
-            if (btn[i].innerHTML.match('<i class="fas fa-plus ouvrir"></i>')) {
-                btn[i].innerHTML = icoferm;
-            }else if (btn[i].innerHTML.match('<i class="fas fa-times fermer"></i>')) {
-                btn[i].innerHTML = '<i class="fas fa-plus ouvrir"></i>';
-            }
-            
-          
-            
-            
-        })
-    };
-
-
-   
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      if (btn[i].innerHTML.match('<i class="fas fa-plus ouvrir"></i>')) {
+        btn[i].innerHTML = icoferm;
+      } else if (btn[i].innerHTML.match('<i class="fas fa-times fermer"></i>')) {
+        btn[i].innerHTML = '<i class="fas fa-plus ouvrir"></i>';
+      }
+    });
+  };
 });
