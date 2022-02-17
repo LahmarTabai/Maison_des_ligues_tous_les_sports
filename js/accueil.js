6e36d601109990de5440a56e878f1ecf95155df0
LahmarTabai <79151540+LahmarTabai@users.noreply.github.com>
2022-02-17 17:46:46 +0100

    modif aria-hidden=true

--- js/accueil.js
+++ js/accueil.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let btn = document.querySelectorAll(".toggle");
     let body = document.querySelector('body');
     let header = document.querySelector('header');
-    let icoferm = '<i class="fas fa-times fermer"></i>';
+    let icoferm = '<i class="fas fa-times fermer" aria-hidden="true"></i>';
 
     for (let i = 0; i < btn.length; i++) {
-        btn[i].addEventListener("click", (e) =>{
-            if (btn[i].innerHTML.match('<i class="fas fa-bars ouvrir"></i>')) {
+        btn[i].addEventListener("click", function (e) {
+            if (btn[i].innerHTML.match('<i class="fas fa-bars ouvrir" aria-hidden="true"></i>')) {
                 btn[i].innerHTML = icoferm;
-                header.style.background = "#000";/* voir le noir */
-            } else if (btn[i].innerHTML.match('<i class="fas fa-times fermer"></i>')) {
-                btn[i].innerHTML = '<i class="fas fa-bars ouvrir"></i>';
+                header.style.background = "#000";
+            } else if (btn[i].innerHTML.match('<i class="fas fa-times fermer" aria-hidden="true"></i>')) {
+                btn[i].innerHTML = '<i class="fas fa-bars ouvrir" aria-hidden="true"></i>';
                 header.style.background = "";
             }
             body.classList.toggle("open");
--- js/app.js
+++ js/app.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let icouvert = document.querySelectorAll("ouvrir");
 
-  let icoferm = '<i class="fas fa-times fermer"></i>';
+  let icoferm = '<i class="fas fa-times fermer" aria-hidden="true"></i>';
 
   for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener("click", function (e) {
 
       res[i].classList.toggle("show");
 
-      if (btn[i].innerHTML.match('<i class="fas fa-plus ouvrir"></i>')) {
+      if (btn[i].innerHTML.match('<i class="fas fa-plus ouvrir" aria-hidden="true"></i>')) {
         btn[i].innerHTML = icoferm;
-      } else if (btn[i].innerHTML.match('<i class="fas fa-times fermer"></i>')) {
-        btn[i].innerHTML = '<i class="fas fa-plus ouvrir"></i>';
+      } else if (btn[i].innerHTML.match('<i class="fas fa-times fermer" aria-hidden="true"></i>')) {
+        btn[i].innerHTML = '<i class="fas fa-plus ouvrir" aria-hidden="true"></i>';
       }
     });
   };
