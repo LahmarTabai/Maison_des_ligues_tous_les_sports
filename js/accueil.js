646ff3ad5442f2055d6e2cd5af6cdd2b480a3e63
LahmarTabai <79151540+LahmarTabai@users.noreply.github.com>
2022-03-02 16:51:38 +0100

    css modif

--- js/accueil.js
+++ js/accueil.js
@@ -1,11 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", e => {
+    e.preventDefault();
     let btn = document.querySelectorAll(".toggle");
     let body = document.querySelector('body');
     let icoferm = '<i class="fas fa-times fermer" aria-hidden="true"></i>';
     let nav = document.querySelector('.gauche');
 
     for (let i = 0; i < btn.length; i++) {
-        btn[i].addEventListener("click", function (e) {
+        btn[i].addEventListener("click", e => {
             if (btn[i].innerHTML.match('<i class="fas fa-bars ouvrir" aria-hidden="true"></i>')) {
                 btn[i].innerHTML = icoferm;
                 nav.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
@@ -16,4 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
             body.classList.toggle("open");
         });
     };
+
+    /*----------------------- Partie Identifiant -------------------------*/
+
+    const local = JSON.parse(localStorage.getItem("user"));
+    let tark = local.identifiant;
+    let ident = tark.substring(0, tark.indexOf('@'));
+
+    let res = document.getElementById("compte");
+
+    res.innerHTML = '<a href="#"><i class="fa fa-user" aria-hidden="true"></i>&nbsp; ' + `${ident}` + "</a>";
 });
